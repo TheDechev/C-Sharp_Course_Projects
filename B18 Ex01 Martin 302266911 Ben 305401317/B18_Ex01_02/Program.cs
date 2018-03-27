b675f8acbbb629f41b376fbd3ba2df4e23fd70ef
TheDechev <dechevmartin@yahoo.com>
2018-03-27 15:38:44 +0300

    Fixed the second and third

--- B18 Ex01 Martin 302266911 Ben 305401317/B18_Ex01_02/Program.cs
+++ B18 Ex01 Martin 302266911 Ben 305401317/B18_Ex01_02/Program.cs
@@ -3,22 +3,53 @@ using System.Text;
 
 namespace B18_Ex01_02
 {
-    class Program
+    public class Program
     {
+
+        private const short k_defaultSandClockSize = 5;
+
 
         public static void Main()
         {
 
-           string charsToPrint= String.Format(
-@"  *****
-   ***  
-    *  
-   *** 
-  *****");
-            Console.WriteLine(charsToPrint);
-            StringBuilder myStr = new StringBuilder("Hello");
-            
+            StringBuilder sandClockToPrint = new StringBuilder();
+
+            CreateSandClock(ref sandClockToPrint, k_defaultSandClockSize);
+            Console.WriteLine(sandClockToPrint);
+
         }
-            
+
+        public static void CreateSandClock(ref StringBuilder io_triangleToPrint, int i_sizeToPrint)
+        {
+            int previousStringLength = 0;
+            StringBuilder singleLineOfStars = new StringBuilder();
+
+            string myStr = new string('*', i_sizeToPrint);
+
+            singleLineOfStars.AppendLine(myStr);
+            io_triangleToPrint.AppendLine(myStr);
+            io_triangleToPrint.AppendLine(myStr);
+
+            for (int i = 1; i <= i_sizeToPrint / 2; i++)
+            {
+
+                previousStringLength += singleLineOfStars.Length;
+                singleLineOfStars.Remove(0, i + 1);
+                singleLineOfStars.Insert(0, " ", i);
+
+                io_triangleToPrint.Insert(previousStringLength, singleLineOfStars);
+
+                if (i != i_sizeToPrint / 2) //won't print one too many * in the middle of the triangle
+                {
+                    io_triangleToPrint.Insert(previousStringLength, singleLineOfStars);
+                }
+                else if (i_sizeToPrint % 2 == 0) // an even number
+                {
+                    io_triangleToPrint.Remove(previousStringLength + i - 1, i + 2);
+                }
+
+            }
+        }
+
     }
 }
--- B18 Ex01 Martin 302266911 Ben 305401317/B18_Ex01_03/Program.cs
+++ B18 Ex01 Martin 302266911 Ben 305401317/B18_Ex01_03/Program.cs
@@ -5,7 +5,7 @@ namespace B18_Ex01_03
 {
     class Program
     {
-        private const string k_Space = " ";
+        
 
         public static void Main()
         {
@@ -13,43 +13,11 @@ namespace B18_Ex01_03
             StringBuilder triangleToPrint = new StringBuilder();
 
             sizeToPrint = int.Parse(Console.ReadLine());
-            createTriangle(ref triangleToPrint, sizeToPrint);
+            B18_Ex01_02.Program.CreateSandClock(ref triangleToPrint, sizeToPrint);
 
             Console.WriteLine(triangleToPrint);
 
         }
-
-        private static void createTriangle(ref StringBuilder io_triangleToPrint, int i_sizeToPrint)
-        {
-            int previousStringLength = 0;
-            StringBuilder singleLineOfStars = new StringBuilder();
-
-            string myStr = new string('*', i_sizeToPrint);
-
-            singleLineOfStars.AppendLine(myStr);
-            io_triangleToPrint.AppendLine(myStr);
-            io_triangleToPrint.AppendLine(myStr);
-
-            for (int i = 1; i <= i_sizeToPrint / 2; i++)
-            {
-
-                previousStringLength += singleLineOfStars.Length;
-                singleLineOfStars.Remove(0, i + 1);
-                singleLineOfStars.Insert(0, k_Space, i);
-
-                io_triangleToPrint.Insert(previousStringLength, singleLineOfStars);
-
-                if (i != i_sizeToPrint / 2) //won't print one too many * in the middle of the triangle
-                {
-                    io_triangleToPrint.Insert(previousStringLength, singleLineOfStars);
-                }
-                else if (i_sizeToPrint % 2 == 0) // an even number
-                {
-                    io_triangleToPrint.Remove(previousStringLength + i - 1, i + 2);
-                }
-
-            }
-        }
-
+        
     }
 }
