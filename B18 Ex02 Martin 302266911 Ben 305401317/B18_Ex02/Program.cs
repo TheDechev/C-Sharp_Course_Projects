09da8c1610dbd144026f4c2ca56c56fd359da9f7
TheDechev <dechevmartin@yahoo.com>
2018-04-26 22:05:52 +0300

    Added the template for the second project

---
+++ B18 Ex02 Martin 302266911 Ben 305401317/B18_Ex02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+
+namespace B18_Ex02
+{
+    class Program
+    {
+        //<insert Checkers game here>
+    }
+}
