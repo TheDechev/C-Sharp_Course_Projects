9ba5fe96cf653e2ec306b00df74312d1632de5c0
TheDechev <dechevmartin@yahoo.com>
2018-05-06 22:01:35 +0300

    Initializing 3rd project

---
+++ B18 Ex03 Martin 302266911 Ben 305401317/B18 Ex03 Martin 302266911 Ben 305401317/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex03_Martin_302266911_Ben_305401317
+{
+    class Program
+    {
+        //insert musah
+    }
+}
