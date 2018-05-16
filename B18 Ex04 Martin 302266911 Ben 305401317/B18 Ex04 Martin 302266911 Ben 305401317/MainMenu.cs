a42b34caaf957c4727d7611943413dc703fed5d0
TheDechev <dechevmartin@yahoo.com>
2018-05-16 16:12:35 +0300

    Initializing project 4

---
+++ B18 Ex04 Martin 302266911 Ben 305401317/B18 Ex04 Martin 302266911 Ben 305401317/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex04_Martin_302266911_Ben_305401317
+{
+    class MainMenu
+    {
+    }
+}
